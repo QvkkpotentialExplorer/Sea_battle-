1508521a0e1a7a29af74faa3a39fdff84c214a6b
user <f@f.f>
2024-01-11 13:38:34 +0300

    bug fix

--- data/no_cells.py
+++ data/no_cells.py
@@ -1,14 +1,12 @@
 import sqlalchemy
 from .db_session import SqlAlchemyBase
-from sqlalchemy import orm
 
 
 class DeathCell(SqlAlchemyBase):
-    __tablename__ = 'on_cells'
+    __tablename__ = 'no_cells'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
     board_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('boards.id'), nullable=True)
     x = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     u = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
-    board = orm.relationship("Board", back_populates='death_cells')
