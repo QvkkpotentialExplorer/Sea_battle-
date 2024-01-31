b6da323e0131f81494852bd9ee35ec5f7731a8fd
https://github.com/QvkkpotentialExplorer/Sea_battle- <maximkorablyov1@gmail.com>
2024-01-31 13:22:10 +0300

    bugfix

--- blueprints/boards/board.py
+++ blueprints/boards/board.py
@@ -20,8 +20,9 @@ def board_list():
         boards = db_sess.query(Board).all()
         return render_template('board_list.html', boards=boards)
     else:
-        boards = db_sess.query(Board).filter(UserOnBoard.user_id== current_user.id).all()
+        boards = db_sess.query(Board).filter(UserOnBoard.user_id == current_user.id).all()
         return render_template('board_list.html', boards=boards)
+
 
 @board.route('/add', methods=['GET', 'POST'])
 @login_required
@@ -35,15 +36,16 @@ def add_board():
             n=form.n.data,
             default_shoots=form.default_shoots.data
         )
+        print(board)
         db_sess.add(board)
         db_sess.commit()
-        return redirect (f'game_room/{board.id}')
+        return redirect(f'game_room/{board.id}')
     return render_template('add_board.html', form=form)
 
 
 @board.route('/game_room/<int:board_id>', methods=['GET', 'POST'])
 @login_required
-def edit_board(board_id: int):
+def edit_board(board_id: int, errors=None):
     if not current_user.is_admin:
         return abort(401)
     add_ship_form = AddShipForm(board_id=board_id)
@@ -74,7 +76,6 @@ def edit_board(board_id: int):
         db_sess.add(ship)
         db_sess.commit()
 
-
         print(add_ship_form.y.data)
         board_render[add_ship_form.y.data][add_ship_form.x.data] = '#'
         print(board_render)
@@ -83,21 +84,23 @@ def edit_board(board_id: int):
                                add_ship_form=add_ship_form,
                                delete_ship_form=delete_ship_form,
                                board=db_sess.get(Board, board_id),
-                               users = users,
-                               user_on_board = user_on_board,
-                               prizes = prizes,
+                               users=users,
+                               errors = errors,
+                               user_on_board=user_on_board,
+                               prizes=prizes,
                                board_render=[''.join(i) for i in board_render],
-                               size = board.n)
+                               size=board.n)
 
     return render_template('game_room.html',
                            add_ship_form=add_ship_form,
-                           delete_ship_form =delete_ship_form,
-                           prizes = prizes,
-                           users = users,
+                           delete_ship_form=delete_ship_form,
+                           prizes=prizes,
+                           users=users,
+                           errors = errors,
                            user_on_board=user_on_board,
                            board=db_sess.get(Board, board_id),
                            board_render=[''.join(i) for i in board_render],
-                           size = board.n)
+                           size=board.n)
 
 
 @board.route('/show/<int:board_id>', methods=['GET', 'POST'])
@@ -109,53 +112,63 @@ def show_board(board_id):
 
     if board is None:
         return abort(404)
-    shoots = db_sess.query(UserOnBoard).filter(UserOnBoard.board_id == board_id,UserOnBoard.user_id == current_user.id).first()
+    shoots = db_sess.query(UserOnBoard).filter(UserOnBoard.board_id == board_id,
+                                               UserOnBoard.user_id == current_user.id).first()
     if shoots is None:
         shoots_data = UserOnBoard(
             user_id=current_user.id,
-            board_id = board_id,
+            board_id=board_id,
             count=board.default_shoots
         )
         db_sess.add(shoots_data)
-    user_on_board = db_sess.query(UserOnBoard).filter(UserOnBoard.user_id == current_user.id,  UserOnBoard.board_id == board_id).first()
+    user_on_board = db_sess.query(UserOnBoard).filter(UserOnBoard.user_id == current_user.id,
+                                                      UserOnBoard.board_id == board_id).first()
     db_sess.commit()
     if not user_on_board.can_join:
         return 'Вы забанены на этой доске!'
     return render_template('show_board.html')
 
-@board.route('/add_user/<int:board_id>/<int:user_id>',methods=['GET', 'POST'])
+
+@board.route('/add_user/<int:board_id>/<int:user_id>', methods=['GET', 'POST'])
 @login_required
-def add_user(board_id: int,user_id: int ):
+def add_user(board_id: int, user_id: int):
     # if not current_user.is_admin:
     #     return abort(401)
     print(db_sess.query(Ship).filter(board_id == Ship.board_id).all())
+    if not (db_sess.query(UserOnBoard).filter(board_id == UserOnBoard.board_id, UserOnBoard.user_id == user_id)):
+        errors = "Еще "
+        if db_sess.query(Ship).filter(board_id == Ship.board_id).all():
+            count = db_sess.query(Board.default_shoots).filter(Board.id == board_id).first()
+            print(count)
+            user_on_board = UserOnBoard(user_id=user_id,
+                                        board_id=board_id,
+                                        can_join=True,
+                                        count=count[0])
 
-    if db_sess.query(Ship).filter(board_id == Ship.board_id).all():
-        count = db_sess.query(Board.default_shoots).filter(Board.id == board_id).first()
-        print(count)
-        user_on_board = UserOnBoard(user_id=user_id,
-                                    board_id = board_id,
-                                    can_join = True,
-                                    count=count[0])
+            db_sess.add(user_on_board)
+            db_sess.commit()
+            return redirect(url_for('board.edit_board', board_id=board_id))
+        else:
+            return redirect(url_for('board.edit_board', board_id=board_id,errors = errors))
 
-        db_sess.add(user_on_board)
-        db_sess.commit()
-        return redirect(url_for('board.edit_board', board_id =board_id))
     else:
-        return redirect(url_for('board.edit_board',board_id=board_id))
+        errors = "Пользователь уже добавлен на поле"
+        return redirect(url_for('board.edit_board', board_id=board_id,errors = errors))
+
 
 @board.route('/delete_user/<int:board_id>/<int:user_id>')
 @login_required
-def delete_user(board_id: int,user_id: int):
+def delete_user(board_id: int, user_id: int):
     if not current_user.is_admin:
         return abort(401)
-    user = db_sess.query(UserOnBoard).filter(UserOnBoard.user_id == user_id,UserOnBoard.board_id ==  board_id).first()
+    user = db_sess.query(UserOnBoard).filter(UserOnBoard.user_id == user_id, UserOnBoard.board_id == board_id).first()
     db_sess.delete(user)
     db_sess.commit()
 
-@board.route('/delete_board/<int:board_id>',methods=['GET', 'POST'])
+
+@board.route('/delete_board/<int:board_id>', methods=['GET', 'POST'])
 @login_required
-def delete_board(board_id:int):
+def delete_board(board_id: int):
     if not current_user.is_admin:
         return abort(401)
     print(board_id)
@@ -165,7 +178,7 @@ def delete_board(board_id:int):
     ships = db_sess.query(Ship).filter(Ship.board_id == board_id).all()
 
     print(ships)
-    if ships: #Проверяем , были ли на поле корабли
+    if ships:  # Проверяем , были ли на поле корабли
         for ship in ships:
             print(ship)
             db_sess.delete(ship)
@@ -180,4 +193,3 @@ def delete_board(board_id:int):
         db_sess.delete(board)
         db_sess.commit()
     return redirect(url_for('board.board_list'))
-
